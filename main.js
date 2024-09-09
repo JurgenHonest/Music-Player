6aaa8516c998bb610afc1ebbc20023380ac78cfe
Sumit Chhetri <sumitchhetri277@gmail.com>
2024-09-09 20:54:25 +0545

    Update main.js

--- main.js
+++ main.js
@@ -18,6 +18,38 @@ function playPause() {
         ctrlIcon.classList.remove("fa-play");
     }
 }
+
+/* Changing the song title and song artist respectively */
+function artic() {
+    songs.src = "media/505.mp3";
+    title.innerHTML = "Artic Monkey";
+    name.innerHTML = "505";
+    playPause();
+}
+function donthink() {
+    songs.src = "media/donthink.mp3";
+    title.innerHTML = "The Surroundings";
+    name.innerHTML = "I Don't think So";
+    playPause();
+}
+function unlove() {
+    songs.src = "media/Unlasting Love.mp3";
+    title.innerHTML = "Lisa";
+    name.innerHTML = "Unlasting Love";
+    playPause();
+}
+function ripTide() {
+    songs.src = "media/Riptide.mp3";
+    title.innerHTML = "Riptide";
+    name.innerHTML = "Vance Joy";
+    playPause();
+}
+function theDevil() {
+    songs.src = "media/Me and the devil.mp3";
+    title.innerHTML = "Me and the Devil";
+    name.innerHTML = "Soap&Skin";
+    playPause();
+}
 if (songs.pause()) {
     setInterval(() => {
         progress.value = songs.currentTime;
